54b3e6e746ea64b306f1f4dbd2c4919e25d62942
anissa <anissa.22kerouaz@gmail.com>
2024-12-03 15:58:35 +0100

    day1

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jobs</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                Jobs
+            </div>
+            <div>
+                links
+            </div>
+            <div>
+                post a job
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+
+
+    </div>
+</body>
+</html>
