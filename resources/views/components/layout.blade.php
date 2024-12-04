74892ad6cc5cc587f5403c620c9b3facae071e79
anissa <anissa.22kerouaz@gmail.com>
2024-12-04 17:07:48 +0100

    day2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,22 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jobs</title>
+    @vite(['resources/css/app.css'])
 </head>
-<body>
-    <div>
-        <nav>
-            <div>
-                Jobs
+<body class = "bg-blue-900 text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center bg-grey-200 py-4 border-b border-white/40">
+            <div class="font-bold">
+                <a href="/">
+                    Jobs.io
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
             </div>
             <div>
-                links
-            </div>
-            <div>
-                post a job
+                <a href="">post a job</a>
             </div>
         </nav>
 
-        <main>
+        <main class="mt-10 max-w-[986px]">
             {{ $slot }}
         </main>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ export default {
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            
         },
     },
     plugins: [],
